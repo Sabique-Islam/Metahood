7a8ece1e01cced1b15f2763509ac33fd7deb8a85
sabique <sabiqueislam7@gmail.com>
2025-08-08 19:03:52 +0530

    feat(Home): add Marquee component to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Navbar } from "@/components/Navbar";
 import { Hero } from "@/components/section/Hero";
+import { Marquee } from "@/components/section/Marquee";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Marquee />
     </div>
   );
 }
